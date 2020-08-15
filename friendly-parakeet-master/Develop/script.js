94960199065b283cd39afad22c1bdcb8038f9c67
terahje <terahjequinn@gmail.com>
2020-08-15 13:53:19 -0700

    added character types and now prints password

--- friendly-parakeet-master/Develop/script.js
+++ friendly-parakeet-master/Develop/script.js
@@ -1,7 +1,18 @@
 // Assignment code here
 
+// Arrays
+var number =["0","1","2","3","4","5","6","7","8","9"];
+var specialChar = ["!", "%","&",",","*","+","-",".","/","<",">","?","~"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+
 // variable Declaration 
 var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumbericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
 
 
 // Prompt to start password generation
@@ -16,6 +27,47 @@ function generatePassword() {
 
   // Repeat back what the user choose for password length 
     alert("Your password will have " + confirmLength + " characters.")
+    
+    // Determine parameters of passwords
+    var confirmSpecialCharacter = confirm("Click OK if you would like to include special characters.");
+    var confirmNumbericCharacter = confirm("Click OK if you would like to include numberic characters.");
+    var confirmLowerCase = confirm("Click OK if you would like to include lowercase characters.");
+    var confirmUpperCase = confirm("Click OK if you would like to include uppercase characters");
+
+  var passwordCharacters = []
+
+  if(confirmSpecialCharacter) {
+    passwordCharacters = passwordCharacters.concat(specialChar)
+  }
+  
+  if(confirmNumbericCharacter){
+    passwordCharacters = passwordCharacters.concat(number)
+  }
+
+  if(confirmLowerCase) {
+    passwordCharacters = passwordCharacters.concat(alphaLower)
+  }
+
+  if(confirmUpperCase) {
+    passwordCharacters = passwordCharacters.concat(alphaUpper)
+  }
+
+  console.log(passwordCharacters)
+
+  // Empty string will fill based on for loop selecting random characters from the array
+  var randomPassword = ""
+
+  for(var i = 0; i < confirmLength; i++){
+    randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+  }
+  return randomPassword;
+
+
+
+
+
+
+
 }
 
 // Get references to the #generate element
