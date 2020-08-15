c6250ea2657bf706541ec667b3be3f18ccaf7c99
terahje <terahjequinn@gmail.com>
2020-08-15 13:10:00 -0700

    added first promptfor passoword length

--- friendly-parakeet-master/Develop/script.js
+++ friendly-parakeet-master/Develop/script.js
@@ -1,5 +1,22 @@
 // Assignment code here
 
+// variable Declaration 
+var confirmLength = "";
+
+
+// Prompt to start password generation
+function generatePassword() {
+  var confirmLength = (prompt("How many characters would you like your password to contain? 8-128 characters."));
+
+  // if answer outside of parameters
+  while(confirmLength <= 7 || confirmLength >= 129) {
+    alert("Password must be between 8-128 characters. Try again.");
+    var confirmLength = (prompt("How many characters would you like your password password to contain?"));
+  }
+
+  // Repeat back what the user choose for password length 
+    alert("Your password will have " + confirmLength + " characters.")
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
